3525e3d1b0b5676ffc14755ee8150b08d0f644ab
Crazie-ash <m.avinashkumar96@gmail.com>
2024-04-22 17:47:16 +0530

    update: 5

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -3,12 +3,15 @@ import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 
+
+
 const Shop = () => {
   return (
     <div>
     <Hero/>
     <Popular/>
     <Offers/>
+    <NewCollection/>
     </div>
   )
 }

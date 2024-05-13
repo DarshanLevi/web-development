c417c4160b3ceaa6f93fca51929d61406eb02499
DarshanLevi <s2darshanrider@gmail.com>
2024-05-13 15:58:52 +0530

    update: 13

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer")
 const path = require("path");
 const cors = require("cors");
-const { error } = require("console");
+const { error, log } = require("console");
 const { type } = require("os");
 const { stringify } = require("querystring");
 
@@ -83,6 +83,35 @@ const Product = mongoose.model("product",{
         type:Boolean,
         default:true
     },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+        {
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        }
+        else{
+            id=1
+        }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    })
+    console.log(product);
+    await product.save();
+    console.log("saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
 })
 
 app.listen(port,(error)=>{

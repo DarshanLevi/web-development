2e7a0b42c6650545f867e3640cd00afad24ce416
DarshanLevi <s2darshanrider@gmail.com>
2024-05-14 12:32:27 +0530

    update: 14

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const multer = require("multer")
+const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const { error, log } = require("console");
@@ -13,112 +13,123 @@ const { stringify } = require("querystring");
 app.use(express.json());
 app.use(cors());
 
-// database connection with mongoDB 
+// database connection with mongoDB
 
-mongoose.connect("mongodb+srv://levi:Priyadarshan2@cluster0.sfebu7f.mongodb.net/e-commerce")
+mongoose.connect(
+  "mongodb+srv://levi:Priyadarshan2@cluster0.sfebu7f.mongodb.net/e-commerce"
+);
 
 // api creation
 
-app.get("/",(req, res)=>{
-    res.send("express app is running")
-
-})
+app.get("/", (req, res) => {
+  res.send("express app is running");
+});
 
 // image storage engine
 
 const storage = multer.diskStorage({
-    destination: './upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage });
 
 // creating upload endpoint for images
 
-app.use('/images',express.static('upload/images'))
+app.use("/images", express.static("upload/images"));
 
-app.post('/upload',upload.single('product'),(req,res)=>{
-    res.json
-    ({
-        success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
-    })
-    
-})
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
 
 // schema for creating products
 
-const Product = mongoose.model("product",{
-    id:{
-        type: Number,
-        required:true,
-    },
-    name:{
-        type:String,
-        required:true,
-    },
-    image:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    new_price:{
-        type:Number,
-        required:true,
-    },
-    old_price:{
-        type:Number,
-        required:true,
-    },
-    date:{
-        type:Date,
-        default:Date.now,
-    },
-    available:{
-        type:Boolean,
-        default:true
-    },
-})
+const Product = mongoose.model("product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-app.post('/addproduct',async (req,res)=>{
-    let products = await Product.find({});
-    let id;
-    if(products.length>0)
-        {
-            let last_product_array = products.slice(-1);
-            let last_product = last_product_array[0];
-            id = last_product.id+1;
-        }
-        else{
-            id=1
-        }
-    const product = new Product({
-        id:id,
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category,
-        new_price:req.body.new_price,
-        old_price:req.body.old_price,
-    })
-    console.log(product);
-    await product.save();
-    console.log("saved");
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("saved");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
+
+// creating API for deleting products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id})
+    console.log('removed');
     res.json({
-        success:true,
-        name:req.body.name,
+        success: true,
+        name:req.body.name
     })
+
 })
 
-app.listen(port,(error)=>{
-    if (!error) { 
-        console.log("server running on port "+ port)
-    }
-    else{
-        console.log("error: "+error)
-    }
-})
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("server running on port " + port);
+  } else {
+    console.log("error: " + error);
+  }
+});

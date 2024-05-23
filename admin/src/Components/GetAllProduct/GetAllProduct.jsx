e68b36a22a94dd9d6291c30958693927fb22ba64
DarshanLevi <s2darshanrider@gmail.com>
2024-05-23 10:07:29 +0530

    update: 19

--- admin/src/Components/GetAllProduct/GetAllProduct.jsx
+++ admin/src/Components/GetAllProduct/GetAllProduct.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./GetAllProduct.css";
+import cross_icon from "../../assets/cross_icon.png"
 
 const GetAllProduct = () => {
-  return (
-    <div className='getAll-product'>
+  
+  const [allproducts, setAllProducts] = useState([]);
 
-    </div>
-  )
+  const fetchInfo = async () => {
+    await fetch("http://localhost:4000/getallproducts")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data)});
+  };
+
+  useEffect(()=>{
+    fetchInfo();
+  },[])
+
+  const remove_product = async (id)=>{
+    await fetch('http://localhost:4000/removeproduct',{
+    method:'POST',
+    headers:{
+      Accept:'application/json',
+      'Content-Type':'application/json',
+    },
+    body:JSON.stringify({id:id})
+  })
+  await fetchInfo();
 }
 
-export default GetAllProduct
+  return (
+    <div className="getAll-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>new price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allproducts.map((product,index)=>{
+          return <><div key={index} className="listproduct-format-main listproduct-format">
+            <img src={product.image} alt="" className="listproduct-product-icon" />
+            <p>{product.name}</p>
+            <p>${product.old_price}</p>
+            <p>${product.new_price}</p>
+            <p>{product.category}</p>
+            <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
+          </div>
+          <hr /></>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default GetAllProduct;
